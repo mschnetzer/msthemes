b735270816e631fa71d5d7735597a8fed46e6310
Matthias Schnetzer <mschnetz@wu.ac.at>
2018-08-30 11:56:38 +0200

    Reorganise theme and create dark color scheme

--- R/theme_ms.R
+++ R/theme_ms.R
@@ -1,17 +1,19 @@
-#' Theme for line plots
+#' Personal theme for ggplot2
 #'
 #' This theme relies on the IBM Plex Sans font which should be installed in advance.
 #' For convenience, run import_ps() to install font.
 #'
 #' @param
 #' grid Whether grid is printed or not, default=TRUE
+#' dark Switch color scheme to dark, default=FALSE
 #' @export
 #' @examples
 #'
 
-theme_ms_line <- function (
+theme_ms <- function (
   base_family="IBMPlexSans", base_size=12,
-  grid=TRUE
+  grid=TRUE,
+  dark=FALSE
 )
 
 {
@@ -22,6 +24,7 @@ theme_ms_line <- function (
       plot.caption = element_text(hjust=1,size = 7),
       axis.ticks = element_blank(),
       legend.background = element_blank(),
+      legend.title=element_text(size=10),
       legend.key = element_blank(),
       legend.text=element_text(size=9),
       panel.background = element_blank(),
@@ -31,11 +34,24 @@ theme_ms_line <- function (
       complete = TRUE
       )
 
+  bgdark <- "#464950"
+
+  # GRID
   if (grid == TRUE) {
     mst <- mst + theme(panel.grid.major=element_line(size=0.2),panel.grid.minor=element_blank())
   } else {
     mst <- mst + theme(panel.grid=element_blank())
   }
-  mst
 
+  # SCHEME
+  if (dark == TRUE) {
+    mst <- mst +
+      theme(plot.background=element_rect(fill=bgdark,color=bgdark),
+            text=element_text(color="white")
+            )
+  } else {
+    mst <- mst
+  }
+
+  mst
 }
