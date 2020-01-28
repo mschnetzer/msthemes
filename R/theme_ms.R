aa2ae90e8f413db164d3f2ac9b401a326db6bfc2
Matthias Schnetzer <mschnetz@wu.ac.at>
2020-01-28 18:35:35 +0100

    White legend text for dark scheme

--- R/theme_ms.R
+++ R/theme_ms.R
@@ -52,7 +52,8 @@ theme_ms <- function (
       theme(plot.background=element_rect(fill=bgdark,color=bgdark),
             text=element_text(color="white"),
             strip.text=element_text(color="white"),
-            axis.text = element_text(color="white")
+            axis.text = element_text(color="white"),
+            legend.text = element_text(color="white")
             )
   } else {
     mst <- mst
